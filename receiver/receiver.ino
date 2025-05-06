b43a79faced999f1abb799da0adec56965c0b29c
Alex Rousseau <roua0701@usherbrooke.ca>
2025-05-06 16:21:20 -0400

    Final

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,8 +1,8 @@
 #include "BLEDevice.h"
-
+ 
 #define TX 13
 #define RX 14
-
+ 
 // Constantes globales
 static BLEUUID serviceUUID("2d6abefe-59a1-4352-b95e-8e1fc2ad8e77");
 static BLEUUID charUUID("a2b6e2b0-c7d7-4eee-b879-61573f8d7c30");
@@ -11,45 +11,44 @@ static boolean connected = false;
 static boolean doScan = false;
 static BLERemoteCharacteristic *pRemoteCharacteristic;
 static BLEAdvertisedDevice *myDevice;
-boolean bluetoothOn = false;
+boolean canReceiveData = false;
 
-// Fonctions
+// Prototypes de fonctions
+String parseValeurs(String data);
+
+
 static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
-  bluetoothOn = true;
-  Serial.print("Notify callback for characteristic ");
-  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
-  Serial.print(" of data length ");
-  Serial.println(length);
-  Serial.print("data: ");
+   Serial.print("data: ");
   Serial.write(pData, length);
   Serial.println();
+  canReceiveData = true;
 }
 
 class MyClientCallback : public BLEClientCallbacks {
   void onConnect(BLEClient *pclient) {}
-
+ 
   void onDisconnect(BLEClient *pclient) {
     connected = false;
-    bluetoothOn = false;
     Serial.println("onDisconnect");
   }
 };
 
+
 bool connectToServer() {
   Serial.print("Forming a connection to ");
   Serial.println(myDevice->getAddress().toString().c_str());
-
+ 
   BLEClient *pClient = BLEDevice::createClient();
   Serial.println(" - Created client");
-
+ 
   pClient->setClientCallbacks(new MyClientCallback());
-
+ 
   // Connect to the remove BLE Server.
-  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  pClient->connect(myDevice);
   Serial.println(" - Connected to server");
-  pClient->setMTU(517);  //set client to request maximum MTU from server (default is 23 otherwise)
-
-  // Obtain a reference to the service we are after in the remote BLE server.
+  pClient->setMTU(517);
+ 
+  // Réference du service
   BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
   if (pRemoteService == nullptr) {
     Serial.print("Failed to find our service UUID: ");
@@ -58,8 +57,8 @@ bool connectToServer() {
     return false;
   }
   Serial.println(" - Found our service");
-
-  // Obtain a reference to the characteristic in the service of the remote BLE server.
+ 
+  // Réference de la caractéristique
   pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
   if (pRemoteCharacteristic == nullptr) {
     Serial.print("Failed to find our characteristic UUID: ");
@@ -68,22 +67,22 @@ bool connectToServer() {
     return false;
   }
   Serial.println(" - Found our characteristic");
-
-  // Read the value of the characteristic.
+ 
+  // Valueur de la caractéristique
   if (pRemoteCharacteristic->canRead()) {
     String value = pRemoteCharacteristic->readValue();
     Serial.print("The characteristic value was: ");
     Serial.println(value.c_str());
   }
-
+ 
   if (pRemoteCharacteristic->canNotify()) {
     pRemoteCharacteristic->registerForNotify(notifyCallback);
   }
-
+ 
   connected = true;
   return true;
 }
-
+ 
 /**
  * Scan les serveurs BLE
  */
@@ -91,24 +90,24 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     Serial.print("BLE Advertised Device found: ");
     Serial.println(advertisedDevice.toString().c_str());
-
+ 
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
-
+ 
       BLEDevice::getScan()->stop();
       myDevice = new BLEAdvertisedDevice(advertisedDevice);
       doConnect = true;
       doScan = true;
-
+ 
     }
   }
 };
-
+ 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1, RX, TX); //Bluetooth
+  Serial1.begin(9600, SERIAL_8N1, RX, TX); // Bluetooth
   Serial.println("Station de base");
-
-  BLEDevice::init("");
+ 
+  BLEDevice::init("Alex - Station de base");
   BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setInterval(1349);
@@ -116,9 +115,9 @@ void setup() {
   pBLEScan->setActiveScan(true);
   pBLEScan->start(5, false);
 }
-
+ 
 void loop() {
-    if (doConnect == true) {
+  if (doConnect == true) {
     if (connectToServer()) {
       Serial.println("Connection établie.");
     } else {
@@ -126,15 +125,53 @@ void loop() {
     }
     doConnect = false;
   }
-
+ 
   if (!connected && doScan) {
     BLEDevice::getScan()->start(0);
   }
-  
-  if (bluetoothOn && Serial1.available()) {
-    String message = Serial1.readStringUntil('\n');
-    Serial.println("Données des capteurs: " + message);
+ 
+  if (canReceiveData && Serial1.available()) {
+    String incomingData = Serial1.readStringUntil('\n');
+    Serial.println("Valeurs de capteurs modifiees.");
+    String donnees = parseValeurs(incomingData);
+    if(incomingData != "" && incomingData != NULL){
+      Serial.println(donnees);
+    }
+    else{
+      Serial.println("Valeurs invalides");
+    }
+    Serial.println("");
+    canReceiveData = false;
+  } else {
+    delay(1000);
+  }
+}
+
+// Retourne une chaine permettant de présenter les valeurs de capteurs avec leurs unités
+String parseValeurs(String data) {
+  float valeurs[8];
+  int i = 0;
+
+  while (data.length() > 0 && i < 8) {
+    int index = data.indexOf(';');
+    if (index == -1) {
+      valeurs[i++] = data.toFloat();
+      break;
+    } else {
+      valeurs[i++] = data.substring(0, index).toFloat();
+      data = data.substring(index + 1);
+    }
   }
 
-  delay(1000);
+  String result = "";
+  result += "Humidité : " + String(valeurs[0], 0) + " %\n";
+  result += "Température (capteur d'humidité) : " + String(valeurs[1], 2) + " °C\n";
+  result += "Intensité lumineuse : " + String(valeurs[2], 2) + " W/m²\n";
+  result += "Température (baromètre) : " + String(valeurs[3], 2) + " °C\n";
+  result += "Pression : " + String(valeurs[4], 2) + " hPa\n";
+  result += "Direction du vent : " + String(valeurs[5], 1) + " °\n";
+  result += "Vitesse du vent : " + String(valeurs[6], 1) + " km/h\n";
+  result += "Précipitations totales : " + String(valeurs[7], 1) + " mm";
+
+  return result;
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -10,13 +10,12 @@
 #define CHARACTERISTIC_UUID "a2b6e2b0-c7d7-4eee-b879-61573f8d7c30"
 #define TX 14
 #define RX 13
+#define PIN_LUMIERE 34
+#define PIN_WIND_DIRECTION 35
+#define PIN_WIND_SPEED 27
+#define PIN_TOTAL_RAIN_FALL 23
 
-// Pins for Weather Carrier with ESP32 Processor Board
-int windDirectionPin = 35;
-int windSpeedPin = 27;
-int rainfallPin = 23;
-
-SFEWeatherMeterKit weatherMeterKit(windDirectionPin, windSpeedPin, rainfallPin);
+SFEWeatherMeterKit weatherMeterKit(PIN_WIND_DIRECTION, PIN_WIND_SPEED, PIN_TOTAL_RAIN_FALL);
 Adafruit_DPS310 dps;
 BLEServer *pServer = NULL;
 BLECharacteristic *pCharacteristic = NULL;
@@ -25,9 +24,164 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
 
+// En-tête de fonctions
 String lectureCapteurs();
+String parseValeurs(String data);
+void temperatureHumidity(float *humidite, float *temperature);
+float intensiteLumineuse();
 
-void temperatureHumidity(float *humidite, float *temperature){
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+  }
+};
+
+void setup() {
+  // Moniteur de la station de capteurs
+  Serial.begin(115200);
+  // Communication UART
+  Serial1.begin(9600, SERIAL_8N1, RX, TX);
+
+  // Configuration des objets dps et weatherMeterKit
+  if (! dps.begin_I2C()) {
+    Serial.println("Failed to find DPS");
+    while (1) yield();
+  }
+  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
+  weatherMeterKit.begin();
+
+ 
+  // Création du périphérique Bluetooth avec Server, Service et Characteristic correspondant
+  BLEDevice::init("Alex - Station de capteurs");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+  );
+
+  // Création du BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+  descriptor_2901 = new BLE2901();
+  descriptor_2901->setDescription("My own description for this characteristic.");
+  descriptor_2901->setAccessPermissions(ESP_GATT_PERM_READ);  // enforce read only - default is Read|Write
+  pCharacteristic->addDescriptor(descriptor_2901);
+  
+  // Activation du service et du advertising
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);
+  BLEDevice::startAdvertising();
+  Serial.println("En attente de connection pour l'envoie de notification...");
+}
+ 
+void loop() {
+  String donnees = lectureCapteurs();
+
+  // Envoi des données des capteurs sur le lien UART et notification BLE
+  if (deviceConnected) {
+    if (donnees != "" && donnees != NULL) {
+      Serial1.println(donnees);
+      Serial.println(parseValeurs(donnees));
+      Serial.println("Données transmises");
+
+      pCharacteristic->setValue((uint8_t *)&value, 4);
+      pCharacteristic->notify();
+      value++;
+    }
+  }
+  
+  // Déconnection
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500);
+    pServer->startAdvertising(); // restart advertising
+    oldDeviceConnected = deviceConnected;
+  }
+  
+  // Connection
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+  }
+}
+
+
+String lectureCapteurs() {
+  sensors_event_t temp_event, pressure_event;
+
+  while (!dps.temperatureAvailable() || !dps.pressureAvailable()) {
+    return ""; // retourne une chaîne vide si les données ne sont pas prêtes
+  }
+  
+  // Lecture de l'humidité (%) et de la température (°C)
+  float humidite = 0;
+  float temperature = 0;
+  temperatureHumidity(&humidite, &temperature);
+
+  // Lecture de l'intensité lumineuse (W/m²)
+  float irradiance = intensiteLumineuse();
+
+  // Lecture de la température (°C) et pression (hPa)
+  dps.getEvents(&temp_event, &pressure_event);
+
+  // Lecture de la direction (°), (km/h) et (mm)
+  float windDir = weatherMeterKit.getWindDirection();
+  float windSpeed = weatherMeterKit.getWindSpeed();
+  float rainfall = weatherMeterKit.getTotalRainfall();
+
+
+  String valeursCapteurs = 
+    String(humidite, 0) + ";" +
+    String(temperature, 2) + ";" +
+    String(irradiance, 2) + ";" +
+    String(temp_event.temperature, 2) + ";" +
+    String(pressure_event.pressure, 2) + ";" +
+    String(windDir, 1) + ";" +
+    String(windSpeed, 1) + ";" +
+    String(rainfall, 1);
+  
+
+  return valeursCapteurs;
+}
+
+// Retourne une chaine permettant de présenter les valeurs de capteurs avec leurs unités
+String parseValeurs(String data) {
+  float valeurs[8];
+  int i = 0;
+
+  while (data.length() > 0 && i < 8) {
+    int index = data.indexOf(';');
+    if (index == -1) {
+      valeurs[i++] = data.toFloat();
+      break;
+    } else {
+      valeurs[i++] = data.substring(0, index).toFloat();
+      data = data.substring(index + 1);
+    }
+  }
+
+  String result = "";
+  result += "Humidité : " + String(valeurs[0], 0) + " %\n";
+  result += "Température (capteur d'humidité) : " + String(valeurs[1], 2) + " °C\n";
+  result += "Intensité lumineuse : " + String(valeurs[2], 2) + " W/m²\n";
+  result += "Température (baromètre) : " + String(valeurs[3], 2) + " °C\n";
+  result += "Pression : " + String(valeurs[4], 2) + " hPa\n";
+  result += "Direction du vent : " + String(valeurs[5], 1) + " °\n";
+  result += "Vitesse du vent : " + String(valeurs[6], 1) + " km/h\n";
+  result += "Précipitations totales : " + String(valeurs[7], 1) + " mm";
+
+  return result;
+}
+
+// Lecture du capteur Température en °C + Humidité %
+void temperatureHumidity(float *humidite, float *temperature) {
   int i, j;
   int duree[42];
   unsigned long pulse;
@@ -65,147 +219,16 @@ void temperatureHumidity(float *humidite, float *temperature){
     }
   }
 
-  if ( (data[0] + data[1] + data[2] + data[3]) != data[4] ) 
+  if ( (data[0] + data[1] + data[2] + data[3]) != data[4] )
     Serial.println(" Erreur checksum");
 
   *humidite = data[0] + (data[1] / 256.0);
   *temperature = data [2] + (data[3] / 256.0);
-  
 }
 
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer *pServer) {
-    deviceConnected = true;
-  };
-  void onDisconnect(BLEServer *pServer) {
-    deviceConnected = false;
-  }
-};
-void setup() {
-  Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1, RX, TX);
-  Serial.println("Station de capteurs");
-
-  if (! dps.begin_I2C()) {
-    Serial.println("Failed to find DPS");
-    while (1) yield();
-  }
-  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
-  dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
-  weatherMeterKit.begin();
-
- 
-  // Create the BLE Device
-  BLEDevice::init("ESP32");
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
-  );
-  // Creates BLE Descriptor 0x2902: Client Characteristic Configuration Descriptor (CCCD)
-  pCharacteristic->addDescriptor(new BLE2902());
-  // Adds also the Characteristic User Description - 0x2901 descriptor
-  descriptor_2901 = new BLE2901();
-  descriptor_2901->setDescription("My own description for this characteristic.");
-  descriptor_2901->setAccessPermissions(ESP_GATT_PERM_READ);  // enforce read only - default is Read|Write
-  pCharacteristic->addDescriptor(descriptor_2901);
-  // Start the service
-  pService->start();
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
-  BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
-}
- 
-void loop() {
-  Serial1.println("Valeurs des capteurs modifiées:");
-  Serial1.println(lectureCapteurs());
-  Serial.println(lectureCapteurs());
-  delay(5000);
-  Serial.println("Données transmises");
- 
-    // notify changed value
-  if (deviceConnected) {
-    pCharacteristic->setValue((uint8_t *)&value, 4);
-    pCharacteristic->notify();
-    value++;
-  }
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-    delay(500);                   // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising();  // restart advertising
-    Serial.println("start advertising");
-    oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
-    oldDeviceConnected = deviceConnected;
-  }
-  delay(1000);
-}
-
-
-String lectureCapteurs() {
-  sensors_event_t temp_event, pressure_event;
-
-  //while (!dps.temperatureAvailable() || !dps.pressureAvailable()) {
-  //  return ""; // retourne une chaîne vide si les données ne sont pas prêtes
-  //}
-
-  float humidite = 0;
-  float temperature = 0;
-  temperatureHumidity(&humidite, &temperature);
-  //Serial.printf(" Humidite = %4.0f %%  Temperature = %4.2f *C \n", humidite, temperature);
-  
-  int lumiere = analogRead(34);
-  float voltage = lumiere * 3.3 / 4095.0;
+// Lecture du capteur d'intensité lumineuse en W/m²
+float intensiteLumineuse() {
+  float voltage = analogRead(PIN_LUMIERE) * 3.3 / 4095.0;
   float lux = pow(voltage / 3.3, 2.0) * 1000.0;
-  float irradiance = lux / 120.0;  // en W/m²
-  //Serial.printf(" Intensité lumineuse = %4.2f W/m²\n", irradiance);
-  
-  dps.getEvents(&temp_event, &pressure_event);
-  //Serial.print(F(" Température du baromètre = "));
-  //Serial.print(temp_event.temperature);
-  //Serial.println(" *C");
-
-  //Serial.print(F(" Pression barométrique = "));
-  //Serial.print(pressure_event.pressure);
-  //Serial.println(" hPa"); 
-
-  // Print data from weather meter kit
-  float windDir = weatherMeterKit.getWindDirection();
-  float windSpeed = weatherMeterKit.getWindSpeed();
-  float rainfall = weatherMeterKit.getTotalRainfall();
-
-  //Serial.print(F("Wind direction (degrees): "));
-  //Serial.print(windDir, 1);
-  //Serial.print(F("\t\t"));
-  //Serial.print(F("Wind speed (kph): "));
-  //Serial.print(windSpeed, 1);
-  //Serial.print(F("\t\t"));
-  //Serial.print(F("Total rainfall (mm): "));
-  //Serial.println(rainfall, 1);
-
-  //Serial.println();
-
-  String valeursCapteurs = 
-    "Humidité : " + String(humidite, 0) + " %\n" +
-    "Température (capteur d'humidité) : " + String(temperature, 2) + " °C\n" +
-    "Intensité lumineuse : " + String(irradiance, 2) + " W/m²\n" +
-    "Température (baromètre) : " + String(temp_event.temperature, 2) + " °C\n" +
-    "Pression : " + String(pressure_event.pressure, 2) + " hPa\n" +
-    "Direction du vent : " + String(windDir, 1) + " °\n" +
-    "Vitesse du vent : " + String(windSpeed, 1) + " km/h\n" +
-    "Précipitations totales : " + String(rainfall, 1) + " mm";
-
-  return valeursCapteurs;
+  return lux / 120.0;
 }
